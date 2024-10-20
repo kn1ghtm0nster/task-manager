f3db48cdb653042879fbdc0ef6ba386ed5348498
Diego Quintanilla <diego.quintanilla@proton.me>
2024-10-20 18:11:28 -0400

    implement dotenv package for server codebase

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import taskRoutes from "./routes/taskRoutes";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -9,5 +11,5 @@ app.use(express.json());
 app.use("/api", taskRoutes); // This is the entry point for all task routes inside taskController.ts
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
