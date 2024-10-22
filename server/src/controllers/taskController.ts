61094bde135f194fd38280ec7d3c8f1634e798d7
kn1ghtm0nster <diego.quintanilla@proton.me>
2024-10-21 20:23:13 -0400

    add errorHandler class to API routes

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -1,14 +1,13 @@
 // LIBRARY IMPORTS - NOT HARD
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 
 // LOCAL IMPORTS - ALSO NOT HARD
 import * as taskService from "../services/taskService";
 import { Task } from "../models/task";
+import { AppError } from "../utils/errorHandler";
 
 // This is where the route logic begins. NOT HARD
-
-// TODO: Implement graceful error handling
 
 /**
  * Get ALL tasks and send them back to the user. NOT HARD
@@ -16,9 +15,17 @@ import { Task } from "../models/task";
  * @param {Request} req - The request object. NOT used here
  * @param {Response} res - The response object used to send ALL tasks
  */
-export const getAllTasks = (req: Request, res: Response): void => {
-  const tasks = taskService.getAllTasks();
-  res.json(tasks);
+export const getAllTasks = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  try {
+    const tasks = taskService.getAllTasks();
+    res.json(tasks);
+  } catch (err) {
+    next(err);
+  }
 };
 
 /**
@@ -29,13 +36,21 @@ export const getAllTasks = (req: Request, res: Response): void => {
  * @param {Request} req - The request object which contains the task ID
  * @param {Response} res - The reponse object, use to send back the task or a 404 status code
  */
-export const getTaskById = (req: Request, res: Response): any => {
-  const task = taskService.getTaskById(req.params.id); // URL param will contain the task id. DON'T FORGET THIS
-  if (!task) {
-    // if task not found, respond with 404. NOT HARD. THIS IS REST DESIGN PLEASE.
-    return res.status(404).json({ message: "TASK NOT FOUND YO" });
+export const getTaskById = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  try {
+    const task = taskService.getTaskById(req.params.id); // URL param will contain the task id. DON'T FORGET THIS
+    if (!task) {
+      // if task not found, respond with 404. NOT HARD. THIS IS REST DESIGN PLEASE.
+      throw new AppError("Task Not Found", 404);
+    }
+    res.json(task);
+  } catch (err) {
+    next(err);
   }
-  res.json(task);
 };
 
 /**
@@ -44,19 +59,27 @@ export const getTaskById = (req: Request, res: Response): any => {
  * @param {Request} req - The request object which contains the new task data
  * @param {Response} res - This is the response object used to send the new task back to the user
  */
-export const createTask = (req: Request, res: Response): any => {
-  const { title, description, status } = req.body;
-  const newTask: Task = {
-    id: uuidv4(),
-    title,
-    description,
-    status: status || "pending",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
+export const createTask = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  try {
+    const { title, description, status } = req.body;
+    const newTask: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: status || "pending",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
 
-  const task = taskService.createTask(newTask);
-  res.status(201).json(task);
+    const task = taskService.createTask(newTask);
+    res.status(201).json(task);
+  } catch (err) {
+    next(err);
+  }
 };
 
 /**
@@ -68,14 +91,22 @@ export const createTask = (req: Request, res: Response): any => {
  *
  * @param {Response} res - The reponse object used to send back the updated task or a 404 status code
  */
-export const updateTask = (req: Request, res: Response): any => {
-  const updatedTask = taskService.updatedTask(req.params.id, req.body); // URL param will contain the task id. DON'T FORGET THIS
+export const updateTask = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  try {
+    const updatedTask = taskService.updatedTask(req.params.id, req.body); // URL param will contain the task id. DON'T FORGET THIS
 
-  if (!updatedTask) {
-    return res.status(404).json({ message: "Task not found" });
+    if (!updatedTask) {
+      throw new AppError("Task Not Found", 404);
+    }
+
+    res.json(updatedTask);
+  } catch (err) {
+    next(err);
   }
-
-  res.json(updatedTask);
 };
 
 /**
@@ -85,12 +116,20 @@ export const updateTask = (req: Request, res: Response): any => {
  *
  * @param {Response} res - The response object that will send a 204 status code if the task is deleted, or a 404 status code if the task is not found.
  */
-export const deleteTask = (req: Request, res: Response): any => {
-  const isDeleted = taskService.deleteTask(req.params.id); // URL param will contain the task id. DON'T FORGET THIS
+export const deleteTask = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  try {
+    const isDeleted = taskService.deleteTask(req.params.id); // URL param will contain the task id. DON'T FORGET THIS
 
-  if (!isDeleted) {
-    return res.status(404).json({ message: "Task not found" });
+    if (!isDeleted) {
+      throw new AppError("Task Not Found", 404);
+    }
+
+    res.status(204).send();
+  } catch (err) {
+    next(err);
   }
-
-  res.status(204).send();
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import taskRoutes from "./routes/taskRoutes";
+import { errorHandler } from "./utils/errorHandler";
 
 dotenv.config();
 const app = express();
@@ -9,7 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api", taskRoutes); // This is the entry point for all task routes inside taskController.ts
+app.use(errorHandler); // This is the error handler middleware
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
