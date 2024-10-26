b267686c21a2ba2bba7e8d1233b0a4d1210eae4a
kn1ghtm0nster <diego.quintanilla@proton.me>
2024-10-26 16:50:07 -0400

    add Link routing component from next to NavBar component

--- client/src/app/components/NavBar.tsx
+++ client/src/app/components/NavBar.tsx
@@ -1,17 +1,16 @@
+import Link from "next/link";
+
 const NavBar = () => (
   <header className="bg-blue-600 text-white p-4">
     <div className="container mx-auto flex justify-between items-center">
       <h1 className="text-lg font-bold">My App</h1>
       <nav className="space-x-4 hidden md:flex">
-        <a href="/" className="hover:text-gray-200">
+        <Link href="/" className="hover:text-gray-200">
           Home
-        </a>
-        <a href="/about" className="hover:text-gray-200">
-          About
-        </a>
-        <a href="/contact" className="hover:text-gray-200">
-          Contact
-        </a>
+        </Link>
+        <Link href="/tasks" className="hover:text-gray-200">
+          Tasks
+        </Link>
       </nav>
       <button className="mg:hidden p-2">Menu</button>
     </div>
