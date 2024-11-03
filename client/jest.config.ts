2c5a2f6af17579c3786a5689e99efe98f73093e2
Diego Quintanilla <diego.quintanilla@proton.me>
2024-11-03 13:51:18 -0500

    update coverage report logic to exclude flies that do not require coverage

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,16 +1,26 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  preset: "ts-jest/presets/js-with-ts-esm",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
-  },
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
+	preset: "ts-jest/presets/js-with-ts-esm",
+	testEnvironment: "jsdom",
+	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+	moduleNameMapper: {
+		"\\.(css|less|scss|sass)$": "identity-obj-proxy",
+	},
+	transform: {
+		"^.+\\.tsx?$": ["ts-jest", { useESM: true }],
+	},
+	extensionsToTreatAsEsm: [".ts", ".tsx"],
+	collectCoverage: true,
+	coverageDirectory: "coverage",
+	collectCoverageFrom: [
+		"src/**/*.{ts,tsx}",
+		"!src/**/*.d.ts",
+		"!src/app/layout.tsx",
+		"!src/app/page.tsx",
+		"!src/**/index.ts",
+		"!src/**/test/**", // Exclude test files if they are in a specific folder
+	],
 };
 
 export default config;
